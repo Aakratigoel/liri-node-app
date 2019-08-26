dcf9504b66c6848f6d3429058c269fff7ce650dc
Aakrati Goel <aakratigoel@gmail.com>
2019-08-26 14:23:28 -0700

    enabled reading from random.txt file

--- liri.js
+++ liri.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
+var fs = require("fs");
 var keys = require("./keys.js");
 var bandsintown = require('bandsintown');
 var spotify = new Spotify(keys.spotify);
 var commandToBeExecuted = process.argv[2];
 var userInput = process.argv[3];
-
-if(commandToBeExecuted === "movie-this")
+function  concertSearch()
+{
+  console.log(userInput);
+    axios.get("https://rest.bandsintown.com/artists/"+userInput+"/events?app_id=codingbootcamp").then(
+      
+        function(response) {
+            
+          console.log("Venue name :"+" " +response.data[0].venue.name);
+            console.log("Venue location:"+ " "+response.data[0].venue.city);
+            console.log("Venue date :"+ " "+response.data[0].datetime);
+        }
+      );
+}
+function movieSearch()
 {
     if(!userInput)
     {
@@ -37,15 +50,14 @@ if(commandToBeExecuted === "movie-this")
               console.log("The movie's Language is : "+response.data.Language);
               console.log("The movie's Plot is : "+response.data.Plot);
               console.log("The movie's Actors are : "+response.data.Actors);
-             // console.log("The movie's Rotten Tomatoes rating is : "+JSON.stringify(response.data.Ratings[1].Value));
+             console.log("The movie's Rotten Tomatoes rating is : "+JSON.stringify(response.data.Ratings[1].Value));
             }
           );
     }
    
 }
-else if(commandToBeExecuted === "spotify-this-song")
+function spotifySearch()
 {
-    
     if(!userInput)
     {
         spotify
@@ -90,18 +102,54 @@ else if(commandToBeExecuted === "spotify-this-song")
           console.log(err);
         });
     }
+}
+if(commandToBeExecuted === "movie-this")
+{
+   movieSearch();
+}
+else if(commandToBeExecuted === "spotify-this-song")
+{
+    
+    spotifySearch();
     
 }
 else if(commandToBeExecuted === "concert-this")
 {
-    axios.get("https://rest.bandsintown.com/artists/"+userInput+"/events?app_id=codingbootcamp").then(
-        function(response) {
-            
-          console.log("Venue name :"+" " +response.data[0].venue.name);
-            console.log("Venue location:"+ " "+response.data[0].venue.city);
-            console.log("Venue date :"+ " "+response.data[0].datetime);
+    concertSearch();
+}
+else if(commandToBeExecuted === "do-what-it-says")
+{
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
         }
-      );
+      
+        // We will then print the contents of data
+        console.log(data);
+      
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+      commandToBeExecuted = dataArr[0];
+      userInput=dataArr[1];
+      if(commandToBeExecuted === "movie-this")
+        {
+            movieSearch();
+        }
+        else if(commandToBeExecuted === "spotify-this-song")
+        {
+    
+             spotifySearch();
+    
+        }
+        else if(commandToBeExecuted === "concert-this")
+        {
+             concertSearch();
+        }
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+      
+      });
 }
 
-// We then run the request with axios module on a URL with a JSON
